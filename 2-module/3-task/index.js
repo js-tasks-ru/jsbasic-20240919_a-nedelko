06d66a9cdb7f97d4b20faf4611e87c9e11fbc334
AleksandrNedelko <a_nedelko@rambler.ru>
2024-10-15 16:43:32 +0300

    2-module 3-task

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,16 @@
 let calculator = {
-  // ваш код
+  read() {
+    this.a = +prompt("Введите чило а?", 0);
+    this.b = +prompt("Введите число  b?", 0);
+  },
+
+  sum() {
+    return this.a + this.b;
+  },
+
+  mul() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
