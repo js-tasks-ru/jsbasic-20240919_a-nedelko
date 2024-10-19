cddac8a2577f9218a6c0a065d4ea3f2362a95c82
AleksandrNedelko <a_nedelko@rambler.ru>
2024-10-19 20:05:35 +0300

    3-module 5-task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,14 @@
 function getMinMax(str) {
-  // ваш код...
+  let num = str
+    .split(" ")
+    .map((item) => parseFloat(item))
+    .filter((item) => !isNaN(item));
+
+  let min = Math.min(...num);
+  let max = Math.max(...num);
+
+  return {
+    min: min,
+    max: max,
+  };
 }
